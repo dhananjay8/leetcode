a5128de7190343ee8155f529f35a95c0c60b0688
Dhananjay <dhananjay.pict@gmail.com>
2025-01-18 22:51:52 +0530

    dp

--- solutions/medium/dp/coin-change.js
+++ solutions/medium/dp/coin-change.js
@@ -13,13 +13,17 @@
 // 3.	Result:
     // •	If dp[amount] is still Infinity, it means it’s not possible to make that amount, so return -1.
     // •	Otherwise, return the value at dp[amount], which represents the minimum number of coins needed.
-    
+
 var coinChange = function (coins, amount) {
   const dp = new Array(amount + 1).fill(Infinity);
   dp[0] = 0; // Base case: 0 coins needed for amount 0
 
   for (const coin of coins) {
+    // For each amount value consider if, including the current coin results in a smaller number of coins required to make up value.
     for (let value = coin; value <= amount; value++) {
+      // •	dp[value] represents the minimum number of coins needed to make up the amount value.
+      // •	dp[value - coin] represents the minimum number of coins needed to make up the amount value - coin (i.e., subtracting the current coin).
+      // •	Adding 1 to dp[value - coin] accounts for using the current coin.
       dp[value] = Math.min(dp[value], dp[value - coin] + 1);
     }
   }

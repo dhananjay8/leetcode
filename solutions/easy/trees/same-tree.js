f1a1c30488ae2b658e55783a9d3e38806b49a6bb
Dhananjay <dhananjay.pict@gmail.com>
2025-01-21 12:33:20 +0530

    Minimum Size Subarray Sum

--- solutions/easy/trees/same-tree.js
+++ solutions/easy/trees/same-tree.js
@@ -1,3 +1,15 @@
+var isSameTree = function (p, q) {
+  if (p == null && q == null) {
+    return true;
+  }
+  if (p == null || q == null || p.val != q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// ALTERNATIVE APPROACH
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
--- solutions/medium/minimum-size-subarray-sum.js
+++ solutions/medium/minimum-size-subarray-sum.js
@@ -4,14 +4,22 @@
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
-  let l = 0;
+  let leftPointer = 0;
   let res = Infinity;
   let currSum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    currSum += nums[i];
+  for (let rightPointer = 0; rightPointer < nums.length; rightPointer++) {
+    // 1.	Outer loop (for):
+    //     •	Traverse the array using i, the right pointer of the window.
+    //     •	Add the current element nums[i] to currSum.
+    // 2.	Inner loop (while):
+    //     •	When currSum is greater than or equal to target, the current subarray [l, i] is valid.
+    //     •	Update res with the smaller value between its current value and the current window size (i + 1 - l).
+    //     •	Shrink the window from the left (l++) by subtracting nums[l] from currSum.
+    //     •	Continue shrinking the window as long as currSum is still >= target, looking for even smaller subarrays.
+    currSum += nums[rightPointer];
     while (currSum >= target) {
-      res = Math.min(res, i + 1 - l);
-      currSum -= nums[l++];
+      res = Math.min(res, rightPointer + 1 - leftPointer);
+      currSum -= nums[leftPointer++];
     }
   }
   return res === Infinity ? 0 : res;

b6883218c769ee0fe186e3a8f06a1beb1e2a832f
Dhananjay <dhananjay.pict@gmail.com>
2025-01-27 16:15:59 +0530

    hamming weight

--- solutions/number-of-1-bits.js
+++ solutions/number-of-1-bits.js
@@ -1,14 +1,15 @@
 /**
- * @param {number} n - a positive integer
+ * @param {number} n
  * @return {number}
  */
 var hammingWeight = function (n) {
-  let count = 0;
-  for (let i = 0; i < 32; i++) {
-    // prettier-ignore
-    if (n &1 === 1) count++;
-    // Shift number to right by 1 i.e. divide by 2
-    n = n >> 1;
+  let count = 0; // Initialize a counter to keep track of set bits (1s)
+
+  // Loop until all bits are processed
+  while (n !== 0) {
+    count += n & 1; // Add 1 to count if the least significant bit (LSB) is set (n & 1 checks LSB)
+    n = n >>> 1; // Perform an unsigned right shift to process the next bit
   }
-  return count;
+
+  return count; // Return the total count of set bits
 };

c6eff1df6b4a0a26db08a1ec754134bede3cb6bf
Dhananjay <dhananjay.pict@gmail.com>
2025-02-03 18:07:50 +0530

    jumps

--- solutions/medium/jump-game-ii.py
+++ solutions/medium/jump-game-ii.py
@@ -1,3 +1,31 @@
+# /**
+#  * @param {number[]} nums
+#  * @return {number}
+#  */
+# var jump = function(nums) {
+#     if(nums.length === 1) return 0;
+#     let minJumps = 0;     // Tracks the number of jumps
+#     let currentEnd = 0;   // The farthest index we can reach in the current jump
+#     let farthest = 0;     // The farthest index reachable overall
+
+#     // Iterate over the array (excluding the last element)
+#     for(let i = 0; i < nums.length-1; i++){
+#         // •	nums[i] tells us how far we can jump from index i.
+# 	    // •	i + nums[i] calculates the maximum index we can potentially reach from i.
+#         farthest = Math.max(farthest, nums[i]+i);
+        
+#         // If we reach the end of the current jump range
+#         if(i === currentEnd){
+#             minJumps++;
+#             currentEnd = farthest; // Move the current range forward
+
+#             // If we can reach or exceed the last index, break early
+#             if(currentEnd >= nums.length-1) break;
+#         }
+#     }
+#     return minJumps;
+# };
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
@@ -19,3 +47,5 @@ class Solution:
                     break
         
         return jumps
+
+
